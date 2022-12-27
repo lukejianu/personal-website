423c06db33cf731673e9b68aca2f7d9b083d2fb3
lukejianu <71729102+lukejianu@users.noreply.github.com>
2022-12-27 12:13:03 -0800

    cleaned up

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { Helmet } from "react-helmet";
+import "./styles.css"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Helmet>
+        <title>Luke Jianu</title>
+      </Helmet>
+      <body>
+        <h1>Luke Jianu</h1>
+        <h2><a href="https://github.com/nikhilsardana">github</a> | <a href="mailto:nsardana@stanford.edu">contact</a></h2>
+        <h2>
+          <p>
+            Northeastern University
+          </p>
+        </h2>
+      </body>
     </div>
   );
 }
 
-export default App;
+export default App; 
