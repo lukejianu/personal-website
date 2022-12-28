6dd22d63b7e476469ca200257547ede5d8e9aeec
lukejianu <71729102+lukejianu@users.noreply.github.com>
2022-12-28 15:02:26 -0800

    more stuff

--- src/App.js
+++ src/App.js
@@ -4,18 +4,21 @@ import "./styles.css"
 function App() {
   return (
     <div>
-      <Helmet>
-        <title>Luke Jianu</title>
-      </Helmet>
-      <body>
-        <h1>Luke Jianu</h1>
-        <h2><a href="https://github.com/lukejianu">github</a> | <a href="mailto:jianuluke@gmail.com">contact</a></h2>
-        <h2>
-          <p>
-            Northeastern University
-          </p>
-        </h2>
-      </body>
+      <div>
+        <Helmet>
+          <title>Luke Jianu</title>
+          <link rel="icon" type="image/png" href="favicon.ico" sizes="16x16" />
+        </Helmet>
+        <body>
+          <h1>Luke Jianu</h1>
+          <h2>
+            <a href="https://github.com/lukejianu" target='_blank'>github</a>
+            &nbsp;| <a href="mailto:jianuluke@gmail.com">contact</a>
+            &nbsp;| <a href="LJ_Resume.pdf" target='_blank' rel="noopener noreferrer">cv</a>
+          </h2>
+          <h2>Northeastern University</h2>
+        </body>
+      </div>
     </div>
   );
 }
