043a266cb0bdb9cb619159bf4567bacc3f6fc663
lukejianu <71729102+lukejianu@users.noreply.github.com>
2022-12-28 21:31:08 -0800

    blah blah blah

--- src/App.js
+++ src/App.js
@@ -14,11 +14,14 @@ function App() {
           <h2>
             <a href="https://github.com/lukejianu" target="_blank" rel="noopener noreferrer">github</a>
             &nbsp;| <a href="mailto:jianuluke@gmail.com">contact</a>
-            &nbsp;| <a href="LJ_Resume.pdf" target="_blank" rel="noopener noreferrer">cv</a>
+            &nbsp;| <a href="LJ_Resume.pdf" type="application/pdf" target="_blank" rel="noopener noreferrer">cv</a>
             &nbsp;| <a href="https://nikhilsardana.github.io/" target="_blank" rel="noopener noreferrer">credit</a>
           </h2>
           <h2>Northeastern University</h2>
         </body>
+      </div>
+      <div>
+        <img src="img.JPG" alt="foofoo" />
       </div>
     </div>
   );
