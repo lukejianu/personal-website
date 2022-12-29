10b875b74ca1cb8862f93a47a391ab7e8b2a3361
lukejianu <71729102+lukejianu@users.noreply.github.com>
2022-12-29 11:58:55 -0800

    changed favicona and changed resume to link

--- src/App.js
+++ src/App.js
@@ -7,14 +7,14 @@ function App() {
       <div>
         <Helmet>
           <title>Luke Jianu</title>
-          <link rel="icon" type="image/png" href="favicon.ico" sizes="16x16" />
+          <link rel="icon" type="image/png" href="favicon3.ico" sizes="16x16" />
         </Helmet>
         <body>
           <h1>Luke Jianu</h1>
           <h2>
             <a href="https://github.com/lukejianu" target="_blank" rel="noopener noreferrer">github</a>
             &nbsp;| <a href="mailto:jianuluke@gmail.com">contact</a>
-            &nbsp;| <a href="https://github.com/lukejianu/personal-website/raw/main/public/LukeJianu_Resume.pdf" target="_blank" rel="noopener noreferrer">cv</a>
+            &nbsp;| <a href="https://drive.google.com/file/d/1QQkqg_NfcTSGPe1-gAUkHBPdJFvbEcG1/view?usp=sharing" target="_blank" rel="noopener noreferrer">cv</a>
             &nbsp;| <a href="https://nikhilsardana.github.io/" target="_blank" rel="noopener noreferrer">credit</a>
           </h2>
           <h2>Northeastern University</h2>
